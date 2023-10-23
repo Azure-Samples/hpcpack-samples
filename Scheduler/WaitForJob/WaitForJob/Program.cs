f4c307450ac77122a68fb575c4998e58863bd0df
tianyiliu <tianyiliu@microsoft.com>
2023-10-23 14:43:22 +0800

    update WaitForJob

--- Scheduler/WaitForJob/WaitForJob/Program.cs
+++ Scheduler/WaitForJob/WaitForJob/Program.cs
@@ -5,9 +5,6 @@
 //Copyright (C) Microsoft Corporation.  All rights reserved.
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
 using System.Threading;
 using Microsoft.Hpc.Scheduler;
 using Microsoft.Hpc.Scheduler.Properties;
@@ -16,6 +13,8 @@ namespace ConsoleApplication3
 {
     class Program
     {
+        const JobState exitStates = JobState.Finished | JobState.Failed | JobState.Canceled;
+
         /// <summary>
         /// Waits for the specified job to reach a terminal state of Finished, Failed or Canceled
         /// </summary>
@@ -23,16 +22,16 @@ namespace ConsoleApplication3
         /// <param name="job"></param>
         static void WaitForJob(IScheduler scheduler, ISchedulerJob job)
         {
-            const JobState exitStates = JobState.Finished | JobState.Failed | JobState.Canceled;
-
             ManualResetEvent checkJobState = new ManualResetEvent(false);
-
             // Event handler for when the job state changes
             EventHandler<JobStateEventArg> jobStatusCheck = (sender, e) =>
             {
-                Console.WriteLine(String.Format("  Job {0} state is now {1}.", job.Id, e.NewState));
+                Console.WriteLine(string.Format("  Job {0} state is now {1}.", job.Id, e.NewState));
+                // Become one of the states
                 if ((e.NewState & exitStates) != 0)
+                {
                     checkJobState.Set();
+                }
             };
 
             // Event handler for when the eventing channel gets reconnected after a failure
@@ -41,31 +40,31 @@ namespace ConsoleApplication3
                 if (e.Code == ConnectionEventCode.EventReconnect)
                 {
                     Console.WriteLine("  Reconnect event detected");
-                    //signal the thread to recheck the job state since the job state event may have been missed
-                    // while we were disconnected.
+                    // Signal the thread to recheck the job state since the job state event may have been missed while we were disconnected.
                     checkJobState.Set();
                 }
                 else
                 {
-                    Console.WriteLine(String.Format("  schedulerConnectionEvent {0}.", e.Code));
+                    Console.WriteLine(string.Format("  schedulerConnectionEvent {0}.", e.Code));
                 }
             };
+            Console.WriteLine(string.Format("Waiting for job {0}...", job.Id));
 
-            Console.WriteLine(String.Format("Waiting for job {0}...", job.Id));
-            
             // Register event handlers before checkJobState is Reset
             job.OnJobState += jobStatusCheck;
             scheduler.OnSchedulerReconnect += schedulerConnectionEvent;
-
+            
             try
             {
                 do
                 {
-                    checkJobState.Reset();  // Always Reset before job.Refresh to avoid losing state transitions
+                    // Always Reset before job.Refresh to avoid losing state transitions
+                    checkJobState.Reset();  
                     job.Refresh();
+
                     if ((job.State & exitStates) != 0)
                     {
-                        Console.WriteLine(String.Format("Job {0} completed with state {1}.", job.Id, job.State));
+                        Console.WriteLine(string.Format("Job {0} completed with state {1}.", job.Id, job.State));
                         return;
                     }
 
@@ -79,29 +78,24 @@ namespace ConsoleApplication3
                 job.OnJobState -= jobStatusCheck;
                 scheduler.OnSchedulerReconnect -= schedulerConnectionEvent;
             }
-
         }
 
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            string headNode = Environment.GetEnvironmentVariable("CCP_SCHEDULER");
+
+            using (IScheduler scheduler = new Scheduler())
             {
-                Console.WriteLine("Usage: WaitForJob <headnode> <test executable>");
-                Console.WriteLine("e.g: WaitForJob myscheduler \"sleep 60\"");
-                Environment.ExitCode = 1;
-                return;
+                scheduler.Connect(headNode);
+
+                ISchedulerJob job = scheduler.CreateJob();
+                ISchedulerTask task = job.CreateTask();
+                task.CommandLine = "ping localhost -n 10";
+                job.AddTask(task);
+                scheduler.SubmitJob(job, null, null);
+
+                WaitForJob(scheduler, job);
             }
-            string headNode = args[0];
-
-            IScheduler scheduler = new Scheduler();
-            scheduler.Connect(headNode);
-
-            ISchedulerJob job = scheduler.CreateJob();
-            ISchedulerTask task = job.CreateTask();
-            task.CommandLine = args[1];
-            job.AddTask(task);
-            scheduler.SubmitJob(job, null, null);
-            WaitForJob(scheduler, job);
         }
     }
 }
--- Scheduler/WaitForJob/WaitForJob/Properties/AssemblyInfo.cs
+++ Scheduler/WaitForJob/WaitForJob/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication3")]
+[assembly: AssemblyTitle("WaitForJob")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("HPC Pack 2016")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HPC Pack 2019")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ecdb30d9-9e90-4182-b89f-d6b3ff33fa02")]
+[assembly: Guid("802c2052-37bc-4101-abc9-c1f54d9f275a")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
