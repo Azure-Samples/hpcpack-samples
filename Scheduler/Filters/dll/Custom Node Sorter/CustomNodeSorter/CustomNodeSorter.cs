953a907bcd59fc2b73884e3fd86651bc18144081
Emily Lo <emilylo@microsoft.com>
2024-11-15 14:40:19 -0500

    Added a sample custom node sorter

---
+++ Scheduler/Filters/dll/Custom Node Sorter/CustomNodeSorter/CustomNodeSorter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Hpc.Scheduler.AddInFilter.HpcClient;
+
+namespace NodeSorter
+{
+    public class NodeSorter : INodeSorter
+    {
+        public int Compare(string nodeX, string nodeY)
+        {
+            var result = nodeY.CompareTo(nodeX);
+            return result;
+        }
+    }
+}
